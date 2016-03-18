700e83cfb369ae8eb74d077658eaf86569ccf683
mcle0341 <mcle0341@algonquinlive.com>
2016-03-18 14:31:10 -0400

    moved the title

--- js/main.js
+++ js/main.js
@@ -2,42 +2,7 @@ var letter = prompt('Enter a letter:');
 
 var i;
 
-for (i = 0; i < 1; i++) {
+for (i = 0; i < 10; i++) {
   document.write(letter + '<br>');
 };
 
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
-};
-
-for (i = 0; i < 1; i++) {
-  document.write(letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
-};
